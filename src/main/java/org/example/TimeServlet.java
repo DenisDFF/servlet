cbdf1afe4cb6928653d5bc9963d490af654a7048
Denis <denis.petrov56245@gmail.com>
2023-11-05 14:26:22 +0100

    servlet complete filter complete fix timeZona +

--- src/main/java/org/example/TimeServlet.java
+++ src/main/java/org/example/TimeServlet.java
@@ -9,8 +9,16 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Time;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.Locale;
 import java.util.TimeZone;
 
 
@@ -18,23 +26,23 @@ import java.util.TimeZone;
 public class TimeServlet extends HttpServlet {
     protected void doGet (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String timezoneParam = request.getParameter("timezone");
-
+        timezoneParam = timezoneParam.replaceAll(" ", "+");
         TimeZone timeZone;
+
+        System.out.println("TimeZone: " + timezoneParam);
+
         if (timezoneParam != null && !timezoneParam.isEmpty()) {
             timeZone = TimeZone.getTimeZone(timezoneParam);
         } else {
-            timeZone = TimeZone.getDefault();
+            timeZone = TimeZone.getTimeZone("UTC");
         }
 
-        SimpleDateFormat dataFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss z");
-        dataFormat.setTimeZone(timeZone);
-
-        String currentTime = dataFormat.format(new Date());
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss z");
+        dateFormat.setTimeZone(timeZone);
+        String currentTime = dateFormat.format(new Date());
 
         response.setContentType("text/html");
-
-        String htmlResponse = "<html><body><h2>Поточний час (" + timeZone.getDisplayName() + "): " + currentTime + "</h2></body></html>";
-
-        response.getWriter().write(htmlResponse);
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>[" + timezoneParam + abc +"]<h2>Поточний час (" + timeZone.getID() + "): " + currentTime + "</h2></body></html>");
     }
 }
